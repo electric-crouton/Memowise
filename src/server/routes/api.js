6e34a8e64d1799d0066854e865b399f2c3068d97
Tegan Duong <teganduong@gmail.com>
2016-06-10 16:18:30 -0700

    Remove authentication from /students route
    - Auth is buggy so need to change before reimplementing in routes

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -8,23 +8,17 @@ import auth from '../controllers/Auth';
 
 const router = new Router();
 
-/*
- * Decks
- */
+/** Decks **/
 router.route('/api/decks').get(auth.checkAuthServer, decks.findAll);
 router.route('/api/decks/courses').get(auth.checkAuthServer, decks.getDecksForStudent); 
 router.route('/api/card').post(auth.checkAuthServer, decks.findNextCard);
 router.route('/api/progress').post(auth.checkAuthServer, decks.progress);
 
-/*
- * Plays
- */
+/** Plays **/
 router.route('/api/play').post(auth.checkAuthServer, plays.create);
 router.route('/api/last-play/deck/:deckId').get(auth.checkAuthServer, plays.findLatest);
 
-/*
- * Courses
- */
+/** Courses **/
 router.route('/api/courses').post(auth.checkAuthServer, courses.addCourse);
 router.route('/api/courses').get(auth.checkAuthServer, courses.getCourses);
 router.route('/api/courses/:courseId/students').post(auth.checkAuthServer, courses.addStudentToCourse);
@@ -32,10 +26,7 @@ router.route('/api/courses/:courseId/students').get(auth.checkAuthServer, course
 router.route('/api/courses/:courseId/decks').post(auth.checkAuthServer, courses.addDeckToCourse); 
 router.route('/api/courses/:courseId/decks').get(auth.checkAuthServer, courses.getDecksForCourse); 
 
-
-/*
- * Auth
- */
+/** Auth **/
 router.route('/api/auth/create-account').post(auth.createAccount);
 router.route('/api/auth/sign-in').post(auth.signIn);
 router.route('/api/auth/verify').get(auth.verify);
