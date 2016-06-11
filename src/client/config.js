79029c430521c6d679984fb84862eb1b6b54df5d
Tegan Duong <teganduong@gmail.com>
2016-06-11 00:14:25 -0700

    Change config files for consistent url

--- src/client/config.js
+++ src/client/config.js
@@ -3,7 +3,7 @@ export const DEBUG = false;
 export const config = {
   api: {
     protocol: process.env.PROTOCOL || 'http',
-    host: process.env.HOST || 'localhost',
+    host: 'localhost',
     port: process.env.PORT || 3000,
   },
 };
