f58a224a1571067b848d3431e6c24cea57e822a9
Tegan Duong <teganduong@gmail.com>
2016-06-11 00:13:36 -0700

    Add navbar to StudentsTab

--- src/client/components/StudentsTab.js
+++ src/client/components/StudentsTab.js
@@ -3,7 +3,7 @@ import AddStudent from '../containers/AddStudent';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { receiveStudents, failedRequest } from '../actions'
-// import CourseTabs from './CourseTabs'
+import CourseTabs from './CourseTabs'
 
 const mapStateToProps = ({ students }) => ({ students });
 // function mapStateToProps(state, ownProps) {
@@ -16,17 +16,18 @@ class StudentsTab extends React.Component {
 
   }
 
-  // componentDidMount() {
-  //   return fetch(`/api/courses/${this.props.params.courseId}/students`, {
-  //     credentials: 'same-origin'
-  //   })
-  //   .then(res => res.json())
-  //   .then(students => this.props.dispatch(receiveStudents(students)))
-  // }
+  componentWillMount() {
+    fetch(`/api/courses/${this.props.params.courseId}/students`, {
+      credentials: 'same-origin'
+    })
+    .then(res => res.json())
+    .then(students => this.props.dispatch(receiveStudents(students)))
+  }
 
   render() {
     return (
       <div className="container">
+        <CourseTabs />
         <h1>
           {this.props.students.courseName}
         </h1>
