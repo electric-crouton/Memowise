7a2dd1ab1c52c1e016cb483b7c56c4e42f3a3065
Tegan Duong <teganduong@gmail.com>
2016-06-11 10:09:48 -0700

    Change link to students tab page

--- src/client/components/CourseTabs.js
+++ src/client/components/CourseTabs.js
@@ -7,7 +7,7 @@ const CourseTabs = ({ courseId }) => (
       <a href="#" className="brand-logo right">Courses</a>
       <ul className="left">
         <li>
-          <Link to="/courses/:courseId/students">Students</Link>
+          <Link to={`/courses/${courseId}/students`}>Students</Link>
         </li>
         <li>
           <Link to={`/courses/${courseId}/decks`}>Decks</Link>
